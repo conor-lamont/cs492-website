7b5f34074adc875c11196b877c22f4da2a6cd696
Conor Lamont <conor_lamont@outlook.com>
2024-04-08 23:19:46 -0400

    pushing comma

--- cs492-website/src/app/mission-statement.js
+++ cs492-website/src/app/mission-statement.js
@@ -11,7 +11,7 @@ export default function MissionStatement() {
                 </p>
                 <h1>The Implementation</h1>
                 <ul className='nice-text'>
-                    <li>We collected insights from students studying at the University of Waterloo, on their thoughts on some of the problems and potential solutions to the problems.</li>
+                    <li>We collected insights from students studying at the University of Waterloo on their thoughts on some of the problems and potential solutions to the problems.</li>
                     <li>We broke down our project into two parts, firstly we did a research survey to collect data on some questions we have come up with.</li>
                     <li>The second part is this website to display our research findings and more</li>
                 </ul>
